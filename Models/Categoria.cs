8cfbb610da1c2bb70fca42171d3c1689ca7d3c15
Caio Santos Portugal <caio.sportugal@hotmail.com>
2021-10-31 01:50:55 -0300

    Atualização de projeto

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -5,6 +5,7 @@ namespace ControleDeEstoque.Models
     public class Categoria
     {
         public int ID { get; set; }
-        public string nomeCategoria { get; set; }
+        public string nome { get; set; }
+        public ICollection<Produto> Produto { get; set; }
     }
 }
