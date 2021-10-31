70d3d3298e140421e3cb6d6739986bcc1c0e91f2
caiosantosp <caio.sportugal@hotmail.com>
2021-10-31 16:21:12 -0300

    CSS atualizado

--- Pages/Produtos/Edit.cshtml.cs
+++ Pages/Produtos/Edit.cshtml.cs
@@ -15,7 +15,7 @@ namespace ControleDeEstoque.Pages.Produtos
     {
         [BindProperty]
         public Produto Produto { get; set; }
-        string baseUrl = "https://localhost:44388";
+        string baseUrl = "https://localhost:44338";
         public async Task<IActionResult> OnGetAsync(int? id)
         {
             if (id == null)
