6a01b9ae7d5e91d592498a8ba8e603766bd1c815
caiosantosp <caio.sportugal@hotmail.com>
2021-11-02 00:59:36 -0300

    Paginas alteradas

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,149 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using ControleDeEstoque.Models;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using Newtonsoft.Json;
-using Microsoft.AspNetCore.Mvc.ViewFeatures;
 
-namespace ControleDeEstoque.Pages
+namespace ControleDeEstoque.Pages.Validacao
 {
-    public class IndexModel : PageModel
+    public class LoginModel : PageModel
     {
-        public List<Produto> Produtos { get; private set; }
-        public List<Produto> MaisVenda { get; private set; }
-        public List<Venda> MaisVendidos { get; private set; }
-        public List<Venda> UltimasAtulizacoes { get; private set; }
-        
-        public int Estoque { get; private set; }
-        public int UltimaSaida { get; private set; }
-
-        string baseUrl = "https://localhost:44338";
-        public async Task OnGetAsync()
+        [BindProperty]
+        public string Usuario { get; set; }
+        [BindProperty]
+        public string Senha { get; set; }
+        public ActionResult OnPostAsync()
         {
-            using (var client = new HttpClient())
+            string userPadrao = "admin";
+            string senhaPadrao = "pass123";
+
+            if (Senha.Equals(senhaPadrao) && Usuario.Equals(userPadrao))
             {
-                client.BaseAddress = new Uri(baseUrl);
-                client.DefaultRequestHeaders.Clear();
-                client.DefaultRequestHeaders.Accept.Add(
-                    new MediaTypeWithQualityHeaderValue("application/json"));
-
-                HttpResponseMessage response = await client.GetAsync("/api/Produto");
-                if (response.IsSuccessStatusCode)
-                {
-                    string result = response.Content.ReadAsStringAsync().Result;
-                    Produtos = JsonConvert.DeserializeObject<List<Produto>>(result);
-                }
-
-                await GetEstoque();
-                await GetMaisVendido();
-                await GetUltimaSaida();
-                await GetUltimasAtulizacoes();
-                await GetMaisVendidos();
+                return RedirectToPage("/Produtos/Index");
             }
-        }
-
-        public async Task GetEstoque()
-        {
-            using (var client = new HttpClient())
+            else
             {
-                client.BaseAddress = new Uri(baseUrl);
-                client.DefaultRequestHeaders.Clear();
-                client.DefaultRequestHeaders.Accept.Add(
-                    new MediaTypeWithQualityHeaderValue("application/json"));
-
-                HttpResponseMessage response = await client.GetAsync("api/Produto/Quantidade-Estoque");
-                if (response.IsSuccessStatusCode)
-                {
-                    string resultEstoque = response.Content.ReadAsStringAsync().Result;
-                    Estoque = JsonConvert.DeserializeObject<int>(resultEstoque);
-                }
+                return RedirectToPage("/Index");
             }
-        }
-
-        public async Task GetMaisVendido()
-        {
-            using (var client = new HttpClient()) { 
-
-                client.BaseAddress = new Uri(baseUrl);
-                client.DefaultRequestHeaders.Clear();
-                client.DefaultRequestHeaders.Accept.Add(
-
-                    new MediaTypeWithQualityHeaderValue("application/json"));
-                HttpResponseMessage response = await client.GetAsync("api/Venda/Melhor-Venda");
-
-                if (response.IsSuccessStatusCode)
-                {
-                    string result = response.Content.ReadAsStringAsync().Result;
-                    MaisVenda = JsonConvert.DeserializeObject<List<Produto>>(result);
-                }
-             }
-        }
-
-        public async Task GetUltimaSaida()
-        {
-            using (var client = new HttpClient())
-            {
-
-                client.BaseAddress = new Uri(baseUrl);
-                client.DefaultRequestHeaders.Clear();
-                client.DefaultRequestHeaders.Accept.Add(
-
-                    new MediaTypeWithQualityHeaderValue("application/json"));
-                HttpResponseMessage response = await client.GetAsync("api/Venda/Ultima-Saida");
-                if (response.IsSuccessStatusCode)
-                {
-                    string result = response.Content.ReadAsStringAsync().Result;
-                    UltimaSaida = JsonConvert.DeserializeObject<int>(result);
-                }
-            }
-        }
-
-        public async Task GetUltimasAtulizacoes()
-        {
-            using (var client = new HttpClient())
-            {
-
-                client.BaseAddress = new Uri(baseUrl);
-                client.DefaultRequestHeaders.Clear();
-                client.DefaultRequestHeaders.Accept.Add(
-
-                    new MediaTypeWithQualityHeaderValue("application/json"));
-                HttpResponseMessage response = await client.GetAsync("api/Venda");
-                if (response.IsSuccessStatusCode)
-                {
-                    string result = response.Content.ReadAsStringAsync().Result;
-                    UltimasAtulizacoes = JsonConvert.DeserializeObject<List<Venda>>(result);
-                }
-            }
-            
-                
-        }
-        public async Task GetMaisVendidos()
-        {
-            using (var client = new HttpClient())
-            {
-
-                client.BaseAddress = new Uri(baseUrl);
-                client.DefaultRequestHeaders.Clear();
-                client.DefaultRequestHeaders.Accept.Add(
-
-                    new MediaTypeWithQualityHeaderValue("application/json"));
-                HttpResponseMessage response = await client.GetAsync("api/Venda");
-                if (response.IsSuccessStatusCode)
-                {
-                    string result = response.Content.ReadAsStringAsync().Result;
-                    MaisVendidos = JsonConvert.DeserializeObject<List<Venda>>(result);
-                }
-            }
-
 
         }
     }
 }
-
